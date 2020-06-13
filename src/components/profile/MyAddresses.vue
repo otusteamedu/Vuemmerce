f432b8a1016ed64364f989301f80ff1dd8c3151f
Dima <dima.avanesov@yandex.ru>
2020-06-13 19:53:58 +0700

    Styled form for adding addresses, list of added addresses

--- src/components/profile/MyAddresses.vue
+++ src/components/profile/MyAddresses.vue
@@ -1,22 +1,33 @@
 <template>
   <div class="container">
-    <form @submit="addAddress">
+    <div class="form-row">
       <input v-model="model.city" id="city" type="text" />
       <label for="city">City</label>
+    </div>
 
+    <div class="form-row">
       <input v-model="model.street" id="street" type="text" />
       <label for="street">Street</label>
+    </div>
 
+    <div class="form-row">
       <input v-model="model.building" id="building" type="number" />
       <label for="building">Building</label>
+    </div>
 
+    <div class="form-row">
       <input v-model="model.apartment" id="apartment" type="number" />
       <label for="apartment">Apartment</label>
+    </div>
 
-      <button type="submit">Add</button>
-    </form>
+    <button class="add-button" @click="addAddress">Add</button>
 
-    <div class="address" v-for="(address, index) in userInfo.addresses" :key="index">{{address}}</div>
+    <div class="address-block">
+      <div class="address" v-for="(address, index) in userInfo.addresses" :key="index">
+        {{address}}
+        <span class="main-address" v-if="index == userInfo.mainAddress">(main)</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -55,4 +66,31 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.container {
+  width: fit-content;
+  padding: 50px;
+
+  .form-row {
+    margin: 5px 0;
+
+    &:last-of-type {
+      margin-bottom: 25px;
+    }
+
+    input {
+      margin-right: 15px;
+      width: 200px;
+    }
+  }
+
+  .add-button {
+    margin-bottom: 50px;
+  }
+
+  .address {
+    .main-address {
+      font-weight: bold;
+    }
+  }
+}
 </style>
