e24e8198cb2c52002eb58dc99725e9845f160958
bogdanovpd@gmail.com <BogdanovPD>
2020-07-22 14:37:57 +0300

    order process improved

--- src/validators.js
+++ src/validators.js
@@ -2,3 +2,8 @@ export function isValidEmail (email) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(email);
 }
+
+export function isValidPhoneNumber (phone) {
+    const re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/
+    return re.test(phone);
+}
